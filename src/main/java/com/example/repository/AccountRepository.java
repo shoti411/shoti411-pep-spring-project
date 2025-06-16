4159f30660233fead1ebcdec604217f5adb3c2f8
Luke Po <lcposhoti411@gmail.com>
2025-06-16 19:47:29 +0000

    added interfaces extending jpa repository

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,12 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import java.util.List;
+
+import com.example.entity.Account;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer>{
+    
 }

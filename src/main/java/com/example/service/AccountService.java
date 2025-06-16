b18d6ab00cedc0c814ec309eab2e0ece4b7b2d25
Luke Po <lcposhoti411@gmail.com>
2025-06-16 20:36:43 +0000

    retrieval functions for MessageService

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,10 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AccountService {
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,38 @@
 package com.example.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MessageService {
+    MessageRepository messageRepository;
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageByMessageId(Integer messageId) {
+        Optional<Message> optionalMessage = messageRepository.findById(messageId);
+        if (optionalMessage.isPresent()) {
+            return optionalMessage.get();
+        } else {
+            return null;
+        }
+    }
+
+    public List<Message> getMessagesByPostedBy(Integer postedBy) {
+        return messageRepository.findMessagesByPostedBy(postedBy);
+    }
 }

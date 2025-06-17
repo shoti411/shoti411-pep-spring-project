f0f25953c4ecd7b5734ed044654bcd19afff354f
Luke Po <lcposhoti411@gmail.com>
2025-06-17 23:24:21 +0000

    persist functions for creating messages and Accounts

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -3,8 +3,26 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    /**
+     * Creates a User Account and saves it to the database
+     * @param account the account to be saved
+     * @return the account being saved
+     */
+    public Account persistAccount(Account account) {
+        return accountRepository.save(account);
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -19,6 +19,15 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
+    /**
+     * Creates a Message to be saved to the database
+     * @param message the message to be saved
+     * @return the message being saved
+     */
+    public Message persistMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
     public List<Message> getAllMessages() {
         return messageRepository.findAll();
     }
@@ -34,5 +43,21 @@ public class MessageService {
 
     public List<Message> getMessagesByPostedBy(Integer postedBy) {
         return messageRepository.findMessagesByPostedBy(postedBy);
+    }
+
+    public void deleteMessageByMessageId(Integer messageId) {
+        messageRepository.deleteById(messageId);
+    }
+
+    public int updateMessageByMessageId(Integer messageId, String messageText) {
+        Optional<Message> optionalMessage = messageRepository.findById(messageId);
+        if(optionalMessage.isPresent()) {
+            Message message = optionalMessage.get();
+            message.setMessageText(messageText);
+            messageRepository.save(message);
+            return 1; // returns 1 because success
+        } else {
+            return 0; // returns 0 because failure
+        }
     }
 }
